45a0510defa07db1ca887dc5918b057305f6d0fa
Marco Chan <inspiredtolive0@gmail.com>
2020-09-24 08:31:42 -0700

    Adds projector interaction

---
+++ Assets/Scripts/TurnOnProjector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+/// <summary>
+/// Provides turning on projector interaction.
+/// </summary>
+[RequireComponent( typeof( Interactable ) )]
+public class TurnOnProjector : MonoBehaviour
+{
+    public GameObject particles;
+    private Interactable interactable;
+    private bool isOn = false;
+
+    void Awake()
+    {
+        interactable = this.GetComponent<Interactable>();
+    }
+
+    private void HandHoverUpdate( Hand hand )
+    {
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+        if (interactable.attachedToHand == null && startingGrabType != GrabTypes.None && isOn == false)
+        {
+            particles.SetActive(true);
+        }
+    }
+}
