42bd2ad274956a901ea24b993aedae92f5a69e5d
Marco Chan <inspiredtolive0@gmail.com>
2020-09-24 08:08:36 -0700

    Adds door opening interaction

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+/// <summary>
+/// Provides door opening interaction.
+/// </summary>
+[RequireComponent( typeof( Interactable ) )]
+public class OpenDoor : MonoBehaviour
+{
+    public Animator doorAnimator;
+    private Interactable interactable;
+    private bool isOpen = false;
+
+    void Awake()
+    {
+        interactable = this.GetComponent<Interactable>();
+    }
+
+    private void HandHoverUpdate( Hand hand )
+    {
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+        if (interactable.attachedToHand == null && startingGrabType != GrabTypes.None && isOpen == false)
+        {
+            doorAnimator.SetBool("character_nearby", true);
+        }
+    }
+}
